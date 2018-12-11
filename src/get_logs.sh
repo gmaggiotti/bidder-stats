a200c0b8d6aea59c5001a461b001fbac3bcf7374
gabe <gmaggiotti@gmail.com>
2018-12-11 18:28:55 -0300

    adding canary & canary-siver

--- src/get_logs.sh
+++ src/get_logs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh ec2-user@172.17.13.230 "tail -$1 /var/log/gunicorn/safirortb.log| grep qps" > ../data/safirortb.log
+ssh ec2-user@$1 "tail -$2 /var/log/gunicorn/safirortb.log| grep qps" > ../data/safirortb.log
