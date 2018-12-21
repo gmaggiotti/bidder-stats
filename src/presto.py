5f94383b3c113196e5441f59767cbe8a7686dede
gabe <gmaggiotti@gmail.com>
2018-12-21 18:33:43 -0300

    Implementing global histogram by region

---
+++ src/presto.py
@@ -0,0 +1,5 @@
+from stats_util import get_presto_bidrate_histogram
+
+hist = get_presto_bidrate_histogram(2018,12,10)
+
+print(hist)
--- src/stats_util.py
+++ src/stats_util.py
@@ -1,3 +1,6 @@
+from pyhive import presto
+import pandas as pd
+
 def enum(**enums):
     return type('Enum', (), enums)
 
@@ -10,3 +13,22 @@ Type = enum(qps_cap=1, qps_eff=2, avg_lat=3, max_lat=4, timeouts=5, bids=6)
 def get_serie(dataset, type, date_from, date_to):
     fn = dataset[:, type]
     return fn
+
+def get_presto_bidrate_histogram(year, month, day):
+    query = """
+     select numeric_histogram(12, cant) as hist, max(cant) as max, min(cant) as min, count(cant) as total 
+     from (  
+             select date_trunc('second', from_iso8601_timestamp(created)), count(*) as cant 
+             from hive.aleph.bids_daily 
+             where day = {} and region = 'ap-southeast-1' 
+             group by 1)
+    """.format("'{:04d}{:02d}{:02d}'".format(year,month,day))
+
+    cursor = presto.connect(host='emr-prd-queries.jampp.com', username='gmaggiotti', port=8889).cursor()
+    cursor.execute(query)
+    cursor.close()
+    hst = cursor.fetchall()
+    df = pd.DataFrame(hst)
+
+    hist, max_, min_, cant = df.iloc[0].values
+    return  sorted([(round(float(k), 2),int(v)) for k,v in hist.items()])
