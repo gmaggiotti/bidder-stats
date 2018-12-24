fixing calculation of mean and standard deviation on histogram

src/presto.py
@@ -1,5 +1,15 @@
 from stats_util import get_presto_bidrate_histogram
+import numpy as np
+#hist = get_presto_bidrate_histogram(24,"20181210","20181210")
+hist = None
+x= [1,2.4,3]
+y= [10,30,10]
 
-hist = get_presto_bidrate_histogram(24,"20181210","20181210")
 
-print(hist)
+n = np.sum(y)
+mean = np.dot(x,y)/n
+standar_dsviation = np.sqrt(np.sum(np.power((np.array(x) - mean), 2) * np.array(y)) / n)
+
+print(mean)
+
+print var